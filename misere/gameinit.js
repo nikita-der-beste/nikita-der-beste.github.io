d3372c98a14f47a156ac788c6664fed8512ff788
nikita-der-beste <96614602+nikita-der-beste@users.noreply.github.com>
2022-03-23 18:53:21 +0100

    Update gameinit.js

--- misere/gameinit.js
+++ misere/gameinit.js
@@ -1,47 +1,35 @@
-var moveCount = 0;
-var lostCount = 0;
+var numfields = 0;
+var board = document.getElementById("board");
 
-function checkField(fld) {
-    var a1 = document.getElementById("a1" + "_" + fld).innerHTML == "X";
-    var a2 = document.getElementById("a2" + "_" + fld).innerHTML == "X";
-    var a3 = document.getElementById("a3" + "_" + fld).innerHTML == "X";
-    var b1 = document.getElementById("b1" + "_" + fld).innerHTML == "X";
-    var b2 = document.getElementById("b2" + "_" + fld).innerHTML == "X";
-    var b3 = document.getElementById("b3" + "_" + fld).innerHTML == "X";
-    var c1 = document.getElementById("c1" + "_" + fld).innerHTML == "X";
-    var c2 = document.getElementById("c2" + "_" + fld).innerHTML == "X";
-    var c3 = document.getElementById("c3" + "_" + fld).innerHTML == "X";
+window.onload = init;
 
-    return (a1 && a2 && a3) || (b1 && b2 && b3) || (c1 && c2 && c3) ||
-        (a1 && b1 && c1) || (a2 && b2 && c2) || (a3 && b3 && c3) ||
-        (a1 && b2 && c3) || (a3 && b2 && c1);
+function getParameterByName(name) {
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(window.location.href);
+    if (!results)
+        return null;
+    if (!results[2])
+        return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-function cross(bttn, fld) {
-    var button = document.getElementById(bttn + "_" + fld);
-    var field = document.getElementById("fld_" + fld);
-
-    if (button.classList.contains("lost") || button.innerHTML == "X")
+function init() {
+    // retrive nf parameter.
+    var nf = getParameterByName("nf");
+    if (!isInt(nf)) {
+        window.location.href = "index.html";
         return;
+    } else
+        numfields = parseInt(nf);
 
-    button.innerHTML = "X";
-    ++moveCount;
+    // generate fields.
+    var i = numfields;
+    for (; i >= 3; i -= 3)
+        board.innerHTML += stack3.replace(/\?/g, i);
 
-    if (checkField(fld)) {
-        Array.from(field.getElementsByClassName("square")).forEach(element => {
-            element.classList.add("lost");
-        });;
-        if (++lostCount == numfields) {
-            document.getElementById("moves").innerHTML = "The game lasted " + moveCount + " moves across " + (lostCount == 1 ? "one board." : (lostCount + " boards."));
-            document.getElementById("gameover").style.visibility = "visible";
-        }
-    }
-}
-
-function reload() {
-    window.location.href = "game.html?nf=" + numfields;
-}
-
-function loadIndex() {
-    window.location.href = "index.html";
+    if (i == 2)
+        board.innerHTML += stack2.replace(/\?/g, i);
+    else if (i == 1)
+        board.innerHTML += stack1.replace(/\?/g, i);
 }
