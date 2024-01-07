36aba9020674313630ecc68f32284aeda9b538b5
nikita-der-beste <96614602+nikita-der-beste@users.noreply.github.com>
2024-01-07 16:03:13 +0100

    Add files via upload

--- poly/rhythm.js
+++ poly/rhythm.js
@@ -4,15 +4,20 @@ var p1 = 0., p2 = 0.;
 var start;
 
 function tick() {
-	const now = (Date.now() - start) * 1.6;
+	const now = (Date.now() - start) * 1.38;
 	
 	document.getElementById("f1").innerHTML = (Math.floor(now * 3 / d1) % 3) + 1;
+											//+ (((Math.floor(now * 12 / d1) % 4) == 0) ? "." : "");
+	//document.getElementById("f1").style.marginTop = Math.floor(10. - 10. * ((now * 3 / d1) % 1)) + "vh";
 	document.getElementById("f2").innerHTML = (Math.floor(now * 3 / d2) % 3) + 1;
+											//+ (((Math.floor(now * 12 / d2) % 4) == 0) ? "." : "");
+	
 }
 
 function start() {
+	alert("Start");
 	start = Date.now();
 	setInterval(tick, 5);
 }
 
-setTimeout(start, 2500);
+start();
